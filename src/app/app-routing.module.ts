b7ffdebb64358a408e2c238b01f97b47954da2f2
giuseppekaligarealvarado <gkaligare@skgtecnologia.com>
2023-04-16 17:57:54 -0500

    [16/05/2023]-[Parametrización estructura front]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import {AuthLayoutComponent} from "./views/auth-layout/auth-layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+      // Else
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full',
+    },
+    // Auth Routes
+    {
+        path: '',
+        component: AuthLayoutComponent,
+        children: [
+            {
+                path: 'auth',
+                loadChildren: () =>
+                    import('./pages/auth/auth.module').then((m) => m.AuthModule),
+            },
+        ],
+    },
+    {
+        path: '**',
+        redirectTo: 'auth/login',
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule,
+            BrowserModule,
+            RouterModule.forRoot(routes, {
+              anchorScrolling: 'enabled',
+              scrollPositionRestoration: 'enabled',
+              scrollOffset: [0, 0],
+            })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
